c3d4dab99a48a4cb15b3211344e4af23c7681af8
nghlong <nghlong011@gmail.com>
2025-02-22 23:50:15 +0700

    4

--- app/Imports/AppTranslationImport.php
+++ app/Imports/AppTranslationImport.php
@@ -11,18 +11,21 @@ class AppTranslationImport implements ToModel, WithHeadingRow
     public function model(array $row)
     {
         // Chỉ nhập dữ liệu vào các cột app_id, lang_id, title, description, details
-        return new AppTranslation([
-            'app_id' => $row['app_id'], // app_id
-            'lang_id' => $row['lang_id'], // lang_id
-            'title' => $row['title'], // title
-            'description' => $row['description'] ?? '', // description, nếu trống thì để là ''
-            'details' => $row['details'] ?? '', // details, nếu trống thì để là ''
-            // Các cột custom_title, custom_description, custom_h1 sẽ không được nhập
-            // Nếu bạn muốn tự động thêm giá trị mặc định cho các cột này, bạn có thể gán giá trị mặc định
+        if (empty($row) || !isset($row['app_id'])) {
+            return null;
+        }
+        $appTranslationData=[
+            'title' => $row['title'],
+            'description' => $row['description'] ?? '',
+            'details' => $row['details'] ?? '',
             'custom_title' => null,
             'custom_description' => null,
             'custom_h1' => null,
-        ]);
+        ];
+        return AppTranslation::updateOrCreate(
+            ['app_id' => $row['app_id'], 'lang_id' => $row['lang_id']],
+            $appTranslationData
+        );
     }
 }
 
